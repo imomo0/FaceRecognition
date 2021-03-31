13730dd9535b1aee45517287c7041d421ac323a5
imomo0 <johkje@gmail.com>
2021-03-31 14:07:05 +0200

    Adding age and gender

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {
@@ -21,11 +22,20 @@ video.addEventListener('play', () => {
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    resizedDetections.forEach( detection => {
+      let gender = "";
+      if(detection.gender === "male") gender = "mann";
+      if(detection.gender === "female") gender = "kvinne";
+      const box = detection.detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age) + " år gammel " + gender })
+      drawBox.draw(canvas)
+      console.log(detection.age)
+    })
   }, 100)
 })
